e69fdfbe2e29eff194ccc664e200a5e945238e28
alaeddinebara <baraalaeddine@gmail.com>
2024-11-06 20:11:22 +0100

    cleaned the code

--- Front-end/src/modules/Dash/UserManagments/User/AllUsersPage.jsx
+++ Front-end/src/modules/Dash/UserManagments/User/AllUsersPage.jsx
@@ -6,15 +6,12 @@ import {SearchX  ,UserRoundCog, Plus , ExternalLink , Ban } from 'lucide-react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useUserContext } from '../../../../context/UserContext';
-import { z } from 'zod';
 import { useNavigate } from 'react-router-dom';
 import PaginationNav from './Components/PaginationNav' 
 
-//validation Shema
-import { UpdateSchema } from './schemas/UpdateSchema';
+
 //components
 import Spinner from '../../../../components//Spinner/Spinner'
-import UpdateUserForm from './components/UpdateUserForm'; 
 import UpdateUserStatusForm from './components/UpdateUserStatusForm';
 import UserCarts from './Components/UserCarts'
 
@@ -25,28 +22,21 @@ const CreateUsers = () => {
     const navigate = useNavigate()
     
     const [formData, setFormData] = useState({
-        firstname: '',
-        lastname: '',
-        username: '',
-        password: '',
-        email: '',
-        gender: '',
+        id: null
     });
 
-    const [errors, setErrors] = useState({});
     const [loading, setLoading] = useState(false);
     const [errorgetdate, setErrorgetdate] = useState(null);
 
     //for get all user
     const [dataUser, setDataUser] = useState([]);
-    const [numberOfData, setnumberOfData] = useState([])
-    ;
+    const [numberOfData, setnumberOfData] = useState([]);
+    
     const [currentPage, setCurrentPage] = useState(1);
     const [limit, setLimit] = useState(10); // Items per page
 
     const totalPages = Math.ceil(numberOfData / limit);
 
-    
     const fetchUsers = useCallback(async (page = 1, limit = 10) => {
         setLoading(true);
         const token = Cookies.get('access_token');
@@ -70,7 +60,7 @@ const CreateUsers = () => {
         }
     }, []);
 
-    // Fetch data on initial render and whenever currentPage or limit changes
+    
     useEffect(() => {
         fetchUsers(currentPage, limit);
     }, [currentPage, limit, fetchUsers]);
@@ -93,127 +83,6 @@ const CreateUsers = () => {
     const startItem = (currentPage - 1) * limit + 1;
     const endItem = Math.min(currentPage * limit, numberOfData);
 
-
-    //for the update 
-    const [isEditing, setIsEditing] = useState(false);
-    const [originalData, setOriginalData] = useState({});
-
-    const [formUpdateData, setUpdateData] = useState({
-        id: null,
-        firstname: '',
-        lastname: '',
-        address: null,
-        phone: null,
-        gender: '',
-    });
-
-    const UpdateGetData = (user) => {
-        setOriginalData(user);
-        setUpdateData({
-            id: user.id,
-            firstname: user.firstname,
-            lastname: user.lastname,
-            address: user.address,
-            phone:  user.phone,
-            gender: user.gender,
-        });
-        setIsEditing(true);
-    };
-
-
-    const handleChangeUpdate = ({ target: { name, value } }) => {
-        setUpdateData((prevData) => ({ ...prevData, [name]: value }));
-    };
-
-    const CloseFormOfUpdate = () => {
-        setIsEditing(false)
-        setUpdateData({
-            id: null,
-            firstname: '',
-            lastname: '',
-            address: null,
-            phone: null,
-            gender: '',
-        });
-        setErrors({});
-    }
-   
-    const updateSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            // Validate the form data against the schema
-            UpdateSchema .parse(formUpdateData);
-    
-            // Create an object to hold the data that will be updated
-            const updatedData = {};
-    
-            // Loop through the keys of formUpdateData
-            for (const key in formUpdateData) {
-                // Only add fields that are changed or are not null
-                if (formUpdateData[key] !== originalData[key]) {
-                    // Set field to null if it's empty
-                    updatedData[key] = formUpdateData[key] ? formUpdateData[key] : null; // Null if empty
-                }
-            }
-    
-            // Optional: Handle case where no fields have changed
-            if (Object.keys(updatedData).length === 0) {
-                toast.info('Aucune mise à jour nécessaire.', {
-                    icon: 'ℹ️',
-                    position: "top-right",
-                    autoClose: 3000,
-                });
-                return;
-            }
-    
-            const token = Cookies.get('access_token');
-
-            const response = await axios.put(
-                `${import.meta.env.VITE_BACKEND_URL}/api/users/${originalData.id}`, 
-                updatedData, // Send only updated values
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-    
-            // Reset form state after a successful update
-            setUpdateData({
-                id: null,
-                firstname: '',
-                lastname: '',
-                address: null,
-                phone: null, 
-                email: '',
-                gender: '',
-            });
-            setErrors({});
-            toast.success('Utilisateur mis à jour avec succès!', {
-                icon: '✅',
-                position: "top-right",
-                autoClose: 3000,
-            });
-            fetchUsers();
-            setIsEditing(false)
-        } catch (error) {
-            if (error instanceof z.ZodError) {
-                const fieldErrors = {};
-                error.errors.forEach(({ path, message }) => {
-                    fieldErrors[path[0]] = message;
-                });
-                setErrors(fieldErrors);
-            } else {
-                console.error('Error updating user:', error.response.data.message);
-                toast.error("Échec de la mise à jour de l'utilisateur.", {
-                    icon: '❌',
-                    position: "top-right",
-                    autoClose: 3000,
-                });
-            }
-        }
-    };
-    
     // for show good formation of last lkogin of user
     const formatDate = (lastLogin) => {
         if (!lastLogin) return "introuvable";
@@ -222,7 +91,6 @@ const CreateUsers = () => {
         const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
         return `${formattedDate} ${formattedTime}`;
     };
-
 
     //for update the status 
     const [status, setStatus] = useState("");
@@ -333,7 +201,6 @@ const CreateUsers = () => {
             </button> 
         </div>
 
-
         {/* Carts Of users */}
         <div>
             {loading ? (
@@ -395,18 +262,6 @@ const CreateUsers = () => {
         )}
         </div>
 
-
-        {/* forum for update user */}
-        {isEditing && (
-            <UpdateUserForm
-                formUpdateData={formUpdateData}
-                handleChangeUpdate={handleChangeUpdate}
-                updateSubmit={updateSubmit}
-                errors={errors}
-                CloseFormOfUpdate={CloseFormOfUpdate}
-            />
-        )}
-
         {/* for update status of user */}
         {isChangeStatus && (
             <UpdateUserStatusForm
@@ -418,7 +273,6 @@ const CreateUsers = () => {
                 statusError={statusError}
             />
         )}
-
     </div>
   );
 };
