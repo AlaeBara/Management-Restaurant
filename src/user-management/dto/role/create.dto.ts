4fefa0a287c23ff784af9db711926ccb190a5a35
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2024-11-07 18:26:17 +0100

    fix: add label/descirption attribute to role dto

--- src/user-management/dto/role/create.dto.ts
+++ src/user-management/dto/role/create.dto.ts
@@ -1,5 +1,6 @@
 import {
   IsNotEmpty,
+  IsOptional,
   Length,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
@@ -8,4 +9,8 @@ export class CreateRoleDto {
   @Length(3, 20)
   @ApiProperty({ description: 'The name of the role', example: 'admin', required: true })
   name: string;
+
+  @IsOptional()
+  @ApiProperty({ description: 'The label of the role', example: 'Accès administratif et gestion', required: false })
+  label: string;
 }
--- src/user-management/dto/role/update.dto.ts
+++ src/user-management/dto/role/update.dto.ts
@@ -1,8 +1,12 @@
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, IsOptional, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 export class UpdateRoleDto {
   @IsNotEmpty()
   @Length(3, 20)
   @ApiProperty({ description: 'The name of the role', example: 'admin', required: true })
   name: string;
+
+  @IsOptional()
+  @ApiProperty({ description: 'The label of the role', example: 'Accès administratif et gestion', required: false })
+  label: string;
 }
