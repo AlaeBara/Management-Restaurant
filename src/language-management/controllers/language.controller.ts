3eacfb24d9be4076952a3ed48665522a924847fe
eeyuub <ayoubfcb2002@gmail.com>
2024-12-31 20:42:16 +0100

    implement fetch all languege Get api

---
+++ src/language-management/controllers/language.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Delete, Get, HttpCode, Inject, Param, ParseUUIDPipe, Patch, Post, Put, Query } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+
+import { LanguageService } from "../services/langague.service";
+import { Permissions, Public } from "src/user-management/decorators/auth.decorator";
+import { Language } from "../entities/language.entity";
+
+
+@Controller('api/languages')
+@ApiTags('language')
+@ApiBearerAuth()
+export class LanguageController {
+    constructor(
+        @Inject()
+        private readonly languageService: LanguageService
+    ) {
+    }
+
+
+    @Get()
+    @Public()
+    @ApiOperation({ summary: 'Get all languages' })
+    async findAll(
+        @Query('page') page?: number,
+        @Query('limit') limit?: number,
+        @Query('relations') relations?: string[],
+        @Query('sort') sort?: string,
+        @Query('withDeleted') withDeleted?: boolean,
+        @Query('onlyDeleted') onlyDeleted?: boolean,
+        @Query('select') select?: string[],
+        @Query() query?: any,
+    ): Promise<{ data: Language[]; total: number; page: number; limit: number }> {
+        return this.languageService.findAll(
+            page,
+            limit,
+            relations,
+            sort,
+            withDeleted,
+            onlyDeleted,
+            select,
+            query,
+        );
+    }
+}
--- src/language-management/language.module.ts
+++ src/language-management/language.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Language } from './entities/language.entity';
 import { LanguageSeeder } from './seeders/language.seeder';
 import { LanguageService } from './services/langague.service';
+import { LanguageController } from './controllers/language.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Language])],
-  controllers: [],
+  controllers: [LanguageController],
   providers: [LanguageSeeder, LanguageService],
   exports: [LanguageSeeder, LanguageService],
 })
