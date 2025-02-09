e73e78ce469e3ad83f42b86cde101371eb8d7b75
alaeddinebara <alaeddinebara17@gmail.com>
2025-02-09 15:21:31 +0100

    update the data time format

--- Front-end/src/components/dateUtils/dateUtils.jsx
+++ Front-end/src/components/dateUtils/dateUtils.jsx
@@ -2,8 +2,20 @@
 
 export const formatDate = (lastLogin) => {
     if (!lastLogin) return "introuvable";
+
     const date = new Date(lastLogin);
-    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
-    const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
-    return `${formattedDate} ${formattedTime}`;
+
+    // Format the date part
+    const optionsDate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let formattedDate = date.toLocaleDateString('fr-FR', optionsDate);
+
+    // Capitalize the first letter of the weekday
+    formattedDate = formattedDate.replace(/^\w/, (match) => match.toUpperCase());
+
+    // Format the time part
+    const optionsTime = { hour: '2-digit', minute: '2-digit' };
+    const formattedTime = date.toLocaleTimeString('fr-FR', optionsTime);
+
+    // Combine date and time
+    return `${formattedDate} - ${formattedTime}`;
 };
