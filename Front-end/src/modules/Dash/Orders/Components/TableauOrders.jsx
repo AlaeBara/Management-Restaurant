99f1071088993ab59f292bf605264ca60a045fa3
alaeddinebara <alaeddinebara17@gmail.com>
2025-02-20 14:04:08 +0100

    update pop up of orders and table

--- Front-end/src/modules/Dash/Orders/Components/TableauOrders.jsx
+++ Front-end/src/modules/Dash/Orders/Components/TableauOrders.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Card, CardContent } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { EyeIcon } from 'lucide-react';
+import { EyeIcon, XIcon, ListCollapse } from 'lucide-react';
 import { formatDate } from '@/components/dateUtils/dateUtils';
+import { Badge } from '@/components/ui/badge';
+
 
 const Tableau = ({ orders }) => {
     const [isDetailsModalVisible, setIsDetailsModalVisible] = useState(false);
@@ -18,7 +20,8 @@ const Tableau = ({ orders }) => {
     return (
         <>
             <Card className='border-none shadow-none'>
-                <CardContent>
+                {/* for remove p-6 */}
+                <CardContent className='p-0'>
                     <Table>
                         <TableHeader className="border bg-gray-100">
                             <TableRow className='hover:bg-transparent'>
@@ -34,10 +37,10 @@ const Tableau = ({ orders }) => {
                             {orders?.length > 0 ? (
                                 orders.map((order) => (
                                     <TableRow key={order.id} className="font-sans">
-                                        <TableCell className="text-center p-4 border">{order.orderNumber}</TableCell>
-                                        <TableCell className="text-center p-4 border">{Number(order.totalAmount)} Dh</TableCell>
+                                        <TableCell className="text-center p-4 border text-nowrap">{order.orderNumber}</TableCell>
+                                        <TableCell className="text-center p-4 border  text-nowrap"> <Badge variant="secondary" className='text-sm rounded-full '>{Number(order.totalAmount)} Dh</Badge> </TableCell>
                                         <TableCell className="text-center p-4 border">{order.orderStatus}</TableCell>
-                                        <TableCell className="text-center p-4 border">{formatDate(order.createdAt)}</TableCell>
+                                        <TableCell className="text-center p-4 border text-nowrap">{formatDate(order.createdAt)}</TableCell>
                                         <TableCell className="text-center p-4 text-nowrap border">
                                             <div className="flex justify-center items-center gap-5 lg:gap-8">
                                                 <button
@@ -67,43 +70,48 @@ const Tableau = ({ orders }) => {
             {isDetailsModalVisible && selectedOrder && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={() => setIsDetailsModalVisible(false)}>
                     <div className="bg-white rounded-lg p-6 max-w-2xl w-full mx-4" onClick={(e) => e.stopPropagation()}>
-                        <h3 className="text-xl font-semibold mb-4 text-center">Détails de la commande</h3>
+
+                        <div className="flex justify-between items-center mb-6">
+                            <h3 className="text-xl font-semibold text-center flex items-center gap-2"> <ListCollapse size={32} /> Détails de la commande</h3>
+                            <button
+                                onClick={() => setIsDetailsModalVisible(false)}
+                                className="p-1 rounded-full bg-gray-100 text-gray-700 hover:bg-gray-200"
+                            >
+                                <XIcon className="h-4 w-4" />
+                            </button>
+                        </div>
+
+
                         <div className="space-y-4">
-                            <p><strong>Numéro de commande :</strong> {selectedOrder.orderNumber}</p>
-                            <p><strong>Montant total :</strong> {Number(selectedOrder.totalAmount)} Dh</p>
-                            <p><strong>Statut de la commande :</strong> {selectedOrder.orderStatus}</p>
-                            <p><strong>Date de création :</strong> {formatDate(selectedOrder.createdAt)}</p>
-                            <p><strong>Table :</strong> {selectedOrder.table?.tableName}</p>
+                            <p className='text-ms'><strong className='text-gray-500'>Numéro de commande :</strong> {selectedOrder.orderNumber}</p>
+                            <p className='text-ms'><strong className='text-gray-500'>Montant total :</strong> <Badge variant="secondary" className='text-sm capitalize rounded-full'>{Number(selectedOrder.totalAmount)} Dh</Badge></p>
+                            <p className='text-ms'><strong className='text-gray-500'>Statut de la commande :</strong> {selectedOrder.orderStatus}</p>
+                            <p className='text-ms'><strong className='text-gray-500'>Date de création :</strong> {formatDate(selectedOrder.createdAt)}</p>
+                            <p className='text-ms'><strong className='text-gray-500'>Table :</strong> {selectedOrder.table?.tableName}</p>
 
                             <h4 className="font-semibold mt-4">Articles de la commande :</h4>
-                            <Table>
-                                <TableHeader>
-                                    <TableRow>
-                                        <TableHead>Produit</TableHead>
-                                        <TableHead>Prix</TableHead>
-                                        <TableHead>Quantité</TableHead>
-                                        <TableHead>Total</TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {selectedOrder.orderItems.map((item) => (
-                                        <TableRow key={item.id}>
-                                            <TableCell>{item.fullLabel}</TableCell>
-                                            <TableCell>{Number(item.price)} Dh</TableCell>
-                                            <TableCell>{item.quantity}</TableCell>
-                                            <TableCell>{Number(item.total)} Dh</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
 
-                            <div className="mt-6 flex justify-end">
-                                <button
-                                    onClick={() => setIsDetailsModalVisible(false)}
-                                    className="px-4 py-2 bg-gray-100 text-gray-700 rounded-md hover:bg-gray-200"
-                                >
-                                    Fermer
-                                </button>
+                            <div className='overflow-y-auto max-h-[300px]'>
+                                <Table>
+                                    <TableHeader>
+                                        <TableRow>
+                                            <TableHead>Produit</TableHead>
+                                            <TableHead>Prix</TableHead>
+                                            <TableHead>Quantité</TableHead>
+                                            <TableHead>Total</TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        {selectedOrder.orderItems.map((item) => (
+                                            <TableRow key={item.id}>
+                                                <TableCell>{item.fullLabel}</TableCell>
+                                                <TableCell>{Number(item.price)} Dh</TableCell>
+                                                <TableCell>{item.quantity}</TableCell>
+                                                <TableCell>{Number(item.total)} Dh</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
                             </div>
                         </div>
                     </div>
